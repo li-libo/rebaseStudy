cfa1e02d5796db78241d4d8d541d7d546da164fd
study1 <study1@qq.com>
2020-06-29 17:13:48 +0800

    first commit

---
+++ logging.groovy
@@ -0,0 +1,28 @@
+scan("30 seconds")
+
+appender("ROLLING", RollingFileAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d %level %thread %mdc %logger{35} - %m%n"
+  }
+  rollingPolicy(SizeAndTimeBasedRollingPolicy) {
+    fileNamePattern = "log/all-%d{yyyy-MM-dd}.%i.log"
+    maxFileSize = "100MB"
+    maxHistory = 30
+    totalSizeCap = "1GB" 
+  }
+}
+appender("ABPMSERVICE", RollingFileAppender) {  
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d %level Thread: %thread - %m%n"
+  } 
+  rollingPolicy(SizeAndTimeBasedRollingPolicy) {
+    fileNamePattern = "log/AbpmService-%d{yyyy-MM-dd}.%i.log"
+    maxFileSize = "100MB"
+    maxHistory = 30
+    totalSizeCap = "1GB" 
+  }
+}
+
+logger("cn.com.agree.abpm.AbpmService",DEBUG,["ABPMSERVICE"],false)
+root(INFO,["ROLLING"])
+
