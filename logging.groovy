2d2d44cd6a26a42974dfbae47b1c173e4d49a0bb
study2 <study2@qq.com>
2020-06-29 18:31:58 +0800

    second study

--- logging.groovy
+++ logging.groovy
@@ -16,13 +16,18 @@ appender("ABPMSERVICE", RollingFileAppender) {
     pattern = "%d %level Thread: %thread - %m%n"
   } 
   rollingPolicy(SizeAndTimeBasedRollingPolicy) {
-    fileNamePattern = "log/AbpmService-%d{yyyy-MM-dd}.%i.log"
+    fileNamePattern = "log/AbpmServiceWithStep-%d{yyyy-MM-dd}.%i.log"
     maxFileSize = "100MB"
     maxHistory = 30
     totalSizeCap = "2GB" 
   }
 }
 
+
 logger("cn.com.agree.abpm.AbpmService",INFO,["ABPMSERVICE"],false)
 root(INFO,["ROLLING"])
+
+logger("cn.com.agree.abpm.AbpmServiceWithStep",DEBUG,["ABPMSERVICE"],false)
+root(DEBUG,["ROLLING"])
+
 
