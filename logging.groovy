e1a5a209915d90176def4b9c9f78a47d18bb5d10
study2 <study2@qq.com>
2020-06-29 18:39:54 +0800

    third commit

--- logging.groovy
+++ logging.groovy
@@ -16,13 +16,25 @@ appender("ABPMSERVICE", RollingFileAppender) {
     pattern = "%d %level Thread: %thread - %m%n"
   } 
   rollingPolicy(SizeAndTimeBasedRollingPolicy) {
-    fileNamePattern = "log/AbpmServiceWithStep-%d{yyyy-MM-dd}.%i.log"
+    fileNamePattern = "log/AbpmService-%d{yyyy-MM-dd}.%i.log"
     maxFileSize = "100MB"
     maxHistory = 30
     totalSizeCap = "1GB" 
   }
 }
+appender("ABPMSERVICEWITHSTEP", RollingFileAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d %level Thread: %thread - %m%n"
+  }
+  rollingPolicy(SizeAndTimeBasedRollingPolicy) {
+    fileNamePattern = "log/AbpmServiceWithStep-%d{yyyy-MM-dd}.%i.log"
+    maxFileSize = "100MB"
+    maxHistory = 30
+    totalSizeCap = "1GB"
+  }
+}
 
-logger("cn.com.agree.abpm.AbpmServiceWithStep",DEBUG,["ABPMSERVICE"],false)
+logger("cn.com.agree.abpm.AbpmService",DEBUG,["ABPMSERVICE"],false)
+logger("cn.com.agree.abpm.AbpmServiceWithStep",DEBUG,["ABPMSERVICEWITHSTEP"],false)
 root(DEBUG,["ROLLING"])
 
